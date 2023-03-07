d61a9c59c456b385c2c955f20004ce31cc25b4e6
renanfulas <renanfulas@outlook.com>
2023-03-06 22:43:22 -0300

    Added 3 pages (Home, Sobre and Contato)

---
+++ recipes/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def contato(request):
+    return render(request, "contato.html")
+
+
+def sobre(request):
+    return render(request, 'sobre.html')
