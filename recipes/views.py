e3817bce1d6a918d08017392645e3a38ba490acf
renanfulas <renanfulas@outlook.com>
2023-03-06 22:52:02 -0300

    Erased contato and sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, "home.html")
-
-
-def contato(request):
-    return render(request, "contato.html")
-
-
-def sobre(request):
-    return render(request, 'sobre.html')
